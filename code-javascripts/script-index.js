de2301a3c0524d43b19468afeed52f9a402aa64f
HunterY74 <96233123+HunterY74@users.noreply.github.com>
2022-01-04 10:34:34 +0100

    Update script-index.js

--- code-javascripts/script-index.js
+++ code-javascripts/script-index.js
@@ -1,12 +1,12 @@
-function createParagraph() {
-  let para = document.createElement('réponse');
-  para.textContent = 'Vous avez cliqué. Le lien en dessous vous mène à la suite du site';
+function createtexte() {
+  let réponse = document.createElement('réponse');
+  réponse.textContent = 'Vous avez cliqué. Le lien en dessous vous mène à la suite du site';
   document.body.appendChild(para);
 }
 
-let button = document.querySelectorAll('button');
+var button = document.querySelectorAll('button');
 
 for(let i = 0; i < buttons.length ; i++) {
-  button[i].addEventListener('click', createParagraph);
+  button[i].addEventListener('click', createtexte);
 }
 
